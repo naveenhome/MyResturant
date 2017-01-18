4b412d5ac71b3a82e63817a9ae0dd7c50fefe7e3
navee <navee@DESKTOP-IAITL0O>
2017-01-18 11:42:26 +0530

    Add project files.

---
+++ MyResturant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyResturant.Startup))]
+namespace MyResturant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
